913cc1e58e7394f8a2070d7cb1271a01b9a17750
vaahxx <vaahxx@gmail.com>
2019-08-11 18:10:09 -0300

    criar classe depoimento e migrations

--- demoday/app/models.py
+++ demoday/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Depoimento(models.Model):
+    empresa = models.CharField(max_length=200)
+    funcionaria_atual = models.BooleanField
+    avaliacao = models.TextField()
+    recomendacao = models.CharField(max_length=200)
+    data = models.DateTimeField(default=timezone.now)
+
+    def publicar(self):
+        self.data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.avaliacao    
