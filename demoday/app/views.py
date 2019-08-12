b3c9e404b876b2b3f930b7185e496f8c391e3a02
vaahxx <vaahxx@gmail.com>
2019-08-12 00:56:18 -0300

    pagina depoimentos com conteudo dinamico

--- demoday/app/views.py
+++ demoday/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Depoimento
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
@@ -12,7 +13,11 @@ def eventos(request):
     return render(request, 'eventos.html')
 
 def depoimentos(request):
-    return render(request, 'depoimentos.html')
+    depoimentos = Depoimento.objects.filter(data__lte=timezone.now()).order_by('data') 
+    return render(request, 'depoimentos.html', {'depoimentos': depoimentos})
+
+# def depoimentos(request):
+#     return render(request, 'depoimentos.html')
 
 def depoimentos_empresas(request):
     return render(request, 'depoimentos-empresa.html')
