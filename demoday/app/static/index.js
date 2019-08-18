0f394eede1b5b5041d3a156d31167ed2c228acdc
thaqnos <thaqnos@gmail.com>
2019-08-18 18:32:17 -0300

    Adicionar função de carrossel na index

---
+++ demoday/app/static/index.js
@@ -0,0 +1,19 @@
+let banner = document.querySelector('.img img');
+let banners = [
+    '../static/images/MULHERES01.png',
+    '../static/images/MULHERES02.png',
+    '../static/images/MULHERES03.png',
+    '../static/images/MULHERES04.png',    
+];
+
+let bannerAtual = 0;
+
+function trocaBanner(){
+    bannerAtual++;
+    if(bannerAtual == banners.length){
+        bannerAtual = 0;
+    }
+    banner.src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
