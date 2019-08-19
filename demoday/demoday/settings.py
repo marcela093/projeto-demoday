43903d0a1a733857742136311748f94020cc392e
valentina <vaahxx@gmail.com>
2019-08-19 13:25:31 -0300

    heroku

--- demoday/demoday/settings.py
+++ demoday/demoday/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,4 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
