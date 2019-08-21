24f2b7982e20a700beb559a748a03d6c64bce7fe
thaqnos <thaqnos@gmail.com>
2019-08-21 16:35:47 -0300

    trocar linha do import django-heroku

--- demoday/demoday/settings.py
+++ demoday/demoday/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,5 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
